9c228a1bdfb156cb237e064178e81a55aabb966e
Sam Martin <mart6353@umn.edu>
2024-07-06 08:24:23 -0500

    cli created and working

--- cli.py
+++ cli.py
@@ -1,20 +1,6 @@
 import interactive as i
 from data_module import data as d
-
-"""
-reset
-get_recipe
-get_recipe_from_ingredient
-calculate_amounts
-  name->use fuzzy find
-  category->[seafood, plants, seasoning]
-  filters->[sushi, recipe]
-upgrade_recipe
-continuous_upgrade
-calculate_recipe
-
-
-"""
+import util as u
 
 choice_mapper = [
   {
@@ -30,13 +16,17 @@ choice_mapper = [
     "function": i.calculate_amounts,
     "options": [
       {
-        "text": "Name",
-        "key": "name",
+        "text": "All Ingredients",
+        "key": "no_arg"
+      },
+      {
+        "text": "Ingredient",
+        "key": "ingredient",
         "type": "fuzzy_ingredient"
       },
       {
         "text": "Recipe",
-        "key": None,
+        "key": "function",
         "function": i.calculate_recipe
       },
       {
@@ -46,10 +36,10 @@ choice_mapper = [
         "options": ["Seafood", "Plants", "Seasoning"]
       },
       {
-        "text": "Filter",
-        "key": "filter",
-        "type": "select",
-        "options": ["Sushi", "Recipe"]
+        "text": "Number of Ingredients",
+        "key": "num_ingredients",
+        "type": "number",
+        "max": 12
       }
     ]
   },
@@ -78,31 +68,70 @@ choice_mapper = [
   }
 ]
 
-def print_options():
-  ...
+def print_options(options):
+  print()
+  for option in options:
+    print(f"{options.index(option) + 1}: {option['text']}")
 
 
 if __name__ == "__main__":
+  first_run = True
   while True:
+    if not first_run:
+      input("Press Enter to continue...")
+    first_run = False
+
     print_options(choice_mapper)
-    choice = input("Enter choice: ")
+    choice = input(f"Enter choice 1-{len(choice_mapper)}: ")
+    print()
     
     try:
-      selection = choice_mapper[int(choice.strip()) + 1]
-    except (ValueError, IndexError):
-      selection = None
+      selection = choice_mapper[int(choice.strip()) - 1]
+    
+      if not selection:
+        print("Invalid choice")
+        print()
+        continue
 
-    if not selection:
-      print("Invalid choice")
-      print()
-      continue
+      if not callable(selection.get('function')):
+        print("Exiting...")
+        break
 
-    if callable(selection.get('function')):
       if not selection.get('options'):
         selection['function']()
-      else:
-        print_options(selection)
+        continue
 
-    else:
-      print("Exiting...")
-      break
+      print_options(selection['options'])
+      option_choice = int(input("Enter choice: ").strip()) - 1
+
+      option = selection['options'][option_choice]
+      key = option['key']
+
+      if key == "no_arg":
+        selection['function']()
+        continue
+
+      if key == "function":
+        option['function']()
+        continue
+
+
+      match option['type']:
+        case "fuzzy_ingredient":
+          argument = u.fuzzy_find_ingredients(input("Enter ingredient: ").strip())
+
+        case "number":
+          argument = int(input("Enter number: "))
+
+        case "select":
+          nested_option = u.choice_picker(option['options'])
+          print()
+          argument = option['options'][nested_option]
+        
+        case _:
+          continue
+
+      selection['function']({key: argument})
+
+    except (ValueError, IndexError):
+      print("Invalid choice")
